99e121f7409d170fb040ba2bce34b20d80697789
Scott Marks <scott@magnolia-heights.com>
2025-07-02 19:37:18 -0400

    csv

---
+++ extract_email_list_from_player_table.py
@@ -0,0 +1,12 @@
+import sqlite3
+con = sqlite3.connect("CarolinaCardClub.db")
+
+res = con.cursor().execute("SELECT * from Player;")
+
+row = res.fetchone()
+while row is not None:
+    (ID, Played_Super_Bowl, Name, Email_address, Phone_number, Other_phone_number_1, Other_phone_number_2, Other_phone_number_3, Prepaid_balance, Player_Category_ID, NickName, Flag) = row
+    if Flag is None: print( Name + " <" + Email_address + ">" )
+    row= res.fetchone()
+
+con.close()
