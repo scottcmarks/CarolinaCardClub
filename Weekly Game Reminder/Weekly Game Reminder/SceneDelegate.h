f8bbbaf5f24d1723e6f8ba43f3b5ccc3d7b0bfb7
Scott Marks <scott@magnolia-heights.com>
2025-07-13 14:18:13 -0400

    Weekly Game Reminder

---
+++ Weekly Game Reminder/Weekly Game Reminder/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Weekly Game Reminder
+//
+//  Created by Scott Marks on 7/10/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
