4358417c6e999f3781634ebf020c827d430d2f3d
Scott Marks <scott@magnolia-heights.com>
2025-07-21 17:01:45 -0400

    updates

--- extract_email_list_from_player_table.py
+++ extract_email_list_from_player_table.py
@@ -6,7 +6,7 @@ res = con.cursor().execute("SELECT * from Player;")
 
 row = res.fetchone()
 while row is not None:
-    (ID, Played_Super_Bowl, Name, Email_address, Phone_number, Other_phone_number_1, Other_phone_number_2, Other_phone_number_3, Prepaid_balance, Player_Category_ID, NickName, Flag) = row
+    (Player_ID, Played_Super_Bowl, Name, Email_address, Phone_number, Other_phone_number_1, Other_phone_number_2, Other_phone_number_3, Player_Category_ID, NickName, Flag) = row
     if Flag is None: print( Name + " <" + Email_address + ">" )
     row= res.fetchone()
 
