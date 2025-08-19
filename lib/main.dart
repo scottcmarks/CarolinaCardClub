39be5e83626a1cfe9573e7bf991a661838e09559
Scott Marks <scott@magnolia-heights.com>
2025-08-19 01:42:57 -0400

    icon dance

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'app_settings_provider.dart';
 import 'database/database_provider.dart';
 import 'time_provider.dart';
 import 'app_settings_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 // Widgets
 import 'main_split_view_page.dart';
@@ -16,7 +18,25 @@ import 'main_split_view_page.dart';
 // import 'app_settings.dart'; // No direct import here, but AppSettingsProvider uses it
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Required to ensure the window manager can be initialized
+  await windowManager.ensureInitialized();
+
+  // Define your initial window size and position
+  WindowOptions windowOptions = WindowOptions(
+    size: const Size(1200, 800), // Desired initial width and height
+    center: true, // Center the window on the screen
+    minimumSize: const Size(800, 600), // Optional: Set a minimum size
+    // You can also set maximumSize, backgroundColor, etc.
+  );
+
+  // Set the window options
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   // Wrap the entire application with MultiProvider to register all top-level providers.
   // This makes these providers accessible from anywhere in your widget tree below this point.
   runApp(
